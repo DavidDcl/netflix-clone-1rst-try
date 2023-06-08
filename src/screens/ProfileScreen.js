7eb22c2cf404bf8f9d8150f57200d960d61eb6e4
DAVID LOPES <davidlopes0208@gmail.com>
2023-06-08 16:58:22 +0200

    First Try Off cloning NetFlix

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './profileScreen.css'
+import Nav from '../Nav'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/counter/userSlice'
+import { auth } from '../fireBase'
+
+function ProfileScreen() {
+  const user = useSelector(selectUser)
+  return (
+    <div className='profileScreen'>
+      <Nav />
+      <div className='profileScreen_body'>
+        <h1>Edit Profile</h1>
+        <div className='profileScreen_info'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+            alt='avatar_logo'
+          />
+          <div className='profileScreen_details'>
+            <h2>{user.email}</h2>
+            <div className='profileScreen_plans'>
+              <h3>Plans</h3>
+              <button
+                onClick={() => auth.signOut()}
+                className='profileScreen_signOut'
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileScreen
